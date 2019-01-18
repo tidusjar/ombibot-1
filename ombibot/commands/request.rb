3ca1148d12e2d59da62073511be153f62531a238
Nick Bell <bell.nicholas.p@gmail.com>
2019-01-17 19:29:18 -0500

    movies working

--- ombibot/commands/request.rb
+++ ombibot/commands/request.rb
@@ -1,8 +1,48 @@
+require "httparty"
+require "uri"
+
+OMBI_API_KEY = ENV["OMBI_API_KEY"]
+OMBI_URL = ENV["OMBI_URL"]
+
 module OmbiBot
   module Commands
     class Request < SlackRubyBot::Commands::Base
-      command "request", "get", "download" do |client, data, match|
-        client.say(text: "Ok, I found this:", channel: data.channel)
+      command "download", "get", "request", "search", "find" do |client, data, match|
+        query = URI.escape(match["expression"])
+        body = JSON.parse(HTTParty.get(
+          "#{OMBI_URL}/api/v1/Search/movie/#{query}",
+          {
+            headers: {
+              "ApiKey" => OMBI_API_KEY,
+            },
+          }
+        ).body)
+
+        build_message = -> {
+          {
+            channel: data.channel,
+            text: "Showing first #{[3, body.size].min} of #{body.size} results for \"#{match["expression"]}\":",
+            fallback: "use a full-featured slack client to use OmbiBot",
+            attachments: body.first(3).map do |movie|
+              {
+                title: "#{movie["title"]} (#{movie["releaseDate"][0..3]})",
+                thumb_url: "https://image.tmdb.org/t/p/w300/#{movie["posterPath"]}",
+                text: movie["overview"][0..200] + "...",
+                callback_id: "movie_request",
+                actions: [
+                  {
+                    name: "request",
+                    text: "Request #{movie["title"]}",
+                    type: "button",
+                    value: movie["id"],
+                  },
+                ],
+              }
+            end,
+          }
+        }
+
+        client.web_client.chat_postMessage(build_message.call)
       end
     end
   end
--- web.rb
+++ web.rb
@@ -1,9 +1,34 @@
 require "sinatra/base"
+require "uri"
+require "httparty"
+
+OMBI_URL = ENV["OMBI_URL"]
+OMBI_API_KEY = ENV["OMBI_API_KEY"]
 
 module OmbiBot
   class Web < Sinatra::Base
     get "/" do
       "Math is good for you."
+    end
+    post "/" do
+      body = JSON.parse(URI.decode(request.body.read)[8..-1])
+      type = body["callback_id"]
+      if type == "movie_request"
+        begin
+          body = {
+            "theMovieDbId" => body["actions"][0]["value"].to_i,
+            "languageCode" => "en",
+          }.to_json
+          res = HTTParty.post(
+            "#{OMBI_URL}/api/v1/Request/movie",
+            headers: {"ApiKey" => OMBI_API_KEY, "Content-Type" => "application/json"},
+            body: body,
+          )
+          return res.parsed_response["message"]
+        rescue => exception
+          puts "exception: " + exception.to_s
+        end
+      end
     end
   end
 end
